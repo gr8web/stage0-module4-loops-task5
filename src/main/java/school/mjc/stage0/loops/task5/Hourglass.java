ff5675d9f00a37fa295f7b84d74f3deb4929e0ee
Grigorij Ibe <2kx.datahell@gmail.com>
2023-01-08 13:24:21 +0100

    nested loops

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,29 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
+        int middle = height / 2;
+        for (int i = 0; i < height; i++) {
 
+            if (i < middle) {
+                for (int j = 0; j < height; j++) {
+                    if (j < i || j >= height - i) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print("8");
+                    }
+                }
+                System.out.println();
+                continue;
+            }
+            int skip = height - i;
+            for (int j = 0; j < height; j++) {
+                if (j < skip - 1 || j > height - skip) {
+                    System.out.print(" ");
+                    continue;
+                }
+                System.out.print("8");
+            }
+            System.out.println();
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -2,6 +2,19 @@ package school.mjc.stage0.loops.task5;
 
 public class Square {
     public void printSquareFrom8s(int sideLength){
-
+        for (int i = 0; i < sideLength; i++) {
+            for (int j = 0; j < sideLength; j++) {
+                if (i == 0 || i == sideLength - 1) {
+                    System.out.print("8");
+                    continue;
+                }
+                if (j == 0 || j == sideLength - 1) {
+                    System.out.print("8");
+                    continue;
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 }
